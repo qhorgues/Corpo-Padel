ece849d19225782fc9b85e4f860644ec41c93fc2
aeliggg <aelig.hamadou@gmail.com>
2025-11-18 17:00:43 +0100

    Use of a button

--- frontend-svelte/src/routes/profile/+page.svelte
+++ frontend-svelte/src/routes/profile/+page.svelte
@@ -1,12 +1,8 @@
-<!-- ============================================
-FICHIER : src/routes/HomePage.svelte
-============================================ -->
 
 <script>
     import { authStore } from "$lib/store/auth.js";
     import profile from "$lib/data/profile_test.json"; 
-    // authStore est supposé être un store Svelte (writable/derived)
-    // Exemple : export const authStore = writable({ isAuthenticated: false, user: null, isAdmin: false });
+    import Page from "../+page.svelte";
 </script>
 
 <div
@@ -29,18 +25,22 @@ FICHIER : src/routes/HomePage.svelte
             </div>
         {:else}
 
-      
-<span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400"><h1>Profil utilisateur</h1></span>
+<div class="flex flex-col items-center gap-6 p-7 rounded-2xl"><span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400"><h1>Profil utilisateur</h1></span></div>    
+
 <div class="flex flex-col items-center gap-6 p-7 rounded-2xl">
   <div>
     <img class="size-48 shadow-xl rounded-md" alt="" src={`${profile.filename}`} />
   </div>
   <div class="flex items-center">
-    <span class="text-2xl font-medium"><p><strong>Nom :</strong> {profile.nom}</p>
+    <span class="text-2xl font-medium">
+    <p><strong>Nom :</strong> {profile.nom}</p>
     <p><strong>Prénom :</strong> {profile.prenom}</p>
     <p><strong>Date de naissance :</strong> {profile.date_naissance}</p>
     <p><strong>Email :</strong> {profile.email}</p>
-    <p><strong>Numéro de licence :</strong> {profile.numero_licence}</p></span>
+    <p><strong>Numéro de licence :</strong> {profile.numero_licence}</p>
+    <button class="mt-4 ml-4 p-2 w-44 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors shadow-md">
+      Éditer les informations
+    </button></span>
     <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
     </span>
   </div>
