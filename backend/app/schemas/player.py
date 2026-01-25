8f55f1959a02f27b501c0fcf77f61db775c51039
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2026-01-25 10:26:05 +0100

    Add player DTOs

---
+++ backend/app/schemas/player.py
@@ -0,0 +1,74 @@
+# app/schemas/player.py
+from datetime import date
+from typing import Optional, List
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class PlayerResponse(BaseModel):
+    """
+    This class is the DTO of a player response.
+    """
+
+    # This is the player's id.
+    id: int
+
+    # This is the player's first name.
+    first_name: str
+
+    # This is the player's last name.
+    last_name: str
+
+    # This is the player's company name.
+    company: str
+
+    # This is the player's license.
+    license_number: str
+
+    # This is the player's birth date.
+    birth_date: Optional[date] = None
+
+    # This is the player's profile picture.
+    photo_url: Optional[str] = None
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+class PlayerRequest(BaseModel):
+    """
+    This class is the DTO of a player request.
+    """
+
+    # This is the player's first name.
+    first_name: str
+
+    # This is the player's last name.
+    last_name: str
+
+    # This is the player's company name.
+    company: str
+
+    # This is the player's license.
+    license_number: str
+
+    # This is the player's mail.
+    email: EmailStr
+
+    # This is the player's password.
+    password: str
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+class PlayersListResponse(BaseModel):
+    """
+    This class is the list of players... Just because in the spec it has.
+    """
+    players: List[PlayerResponse]
+    total: int
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
