c7eda265e06ed47e169ed4336d601024cb39955c
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2026-01-27 00:07:30 +0100

    Add authguard to the project

--- frontend-svelte/src/routes/+layout.svelte
+++ frontend-svelte/src/routes/+layout.svelte
@@ -3,9 +3,18 @@
     import NavBar from "$lib/components/NavBar.svelte";
     import { authStore } from "$lib/store/auth";
     import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    import { page } from "$app/state";
 
     onMount(() => {
         authStore.checkAuth();
+
+        if (page.url.pathname !== "/" && !$authStore.isAuthenticated) {
+            goto('/login');
+        }
+        else if (page.url.pathname === "/login") {
+            goto("/");
+        }
     });
 </script>
 
